b69b2f1023d9f6356a78e94248a8f0541ac40fbb
Dmitry Kiselev <mister.garry75@yandex.ru>
2025-01-14 21:59:17 +0300

    Первый коммит в новой ветке. Работа согласно ТЗ 6 спринта.

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+    void rmv(int id);
     List<Task> getHistory();
 }
---
+++ src/managers/Node.java
@@ -0,0 +1,34 @@
+package managers;
+import tasks.Task;
+
+public class Node {
+    private final Task data;
+    private Node previousNode;
+    private Node nextNode;
+
+    public Node(Task data, Node previousNode) {
+        this.data = data;
+        this.previousNode = previousNode;
+    }
+
+
+    public Task getData() {
+        return data;
+    }
+
+    public Node getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
